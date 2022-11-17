8b307e3b454b00125c481dfd9ea89877825b29d9
SangHyeon5163 <tkdgusska3241@naver.com>
2022-11-17 11:54:40 +0900

    log init

--- translation/0_pdf_to_txt.py
+++ translation/0_pdf_to_txt.py
@@ -1,6 +1,8 @@
-from pdfminer.pdfinterp import PDFResourceManager, process_pdf
+#from pdfminer.pdfinterp import PDFResourceManager, process_pdf
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
 from io import StringIO 
 from io import open 
 from urllib.request import urlopen
@@ -10,11 +12,23 @@ def read_pdf_file(pdfFile):
     retstr = StringIO() 
     laparams = LAParams() 
     device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+    password = "" 
+    maxpages = 0 
+    caching = True
+    pagenos=set()
 
-    process_pdf(rsrcmgr, device, pdfFile)
-    device.close() 
+    #process_pdf(rsrcmgr, device, pdfFile)
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
 
+
+    #device.close() 
+    
+    for page in PDFPage.get_pages(pdf_file, pagenos, maxpages=maxpages, password=password, caching=caching,
+            check_extractable=True): 
+        interpreter.process_page(page)
     content = retstr.getvalue() 
+    
+    device.close()
     retstr.close() 
     return content
 
